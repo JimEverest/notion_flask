e1d3dee05e0ce0c6c136452785518428126c83af
jimeverest <tianwai263@gmail.com>
2024-11-05 09:47:10 +0800

    update

--- app/routes.py
+++ app/routes.py
@@ -357,7 +357,7 @@ def duplicate_page(page_id):
         print(f"Error duplicating page {page_id}: {e}")
         return jsonify({'success': False})
 
-@app.route('/create_sub_page/', methods=['POST'])
+@app.route('/newpag/', methods=['POST'])
 def create_sub_page():
     data = request.json
     parent_id = data.get('parent_id')
